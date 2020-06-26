74b128fed0b0ae90cc416b486871ca258e759a66
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-26 16:55:29 -0400

    Splash attacks nearby enemies

--- src/scripts/TowerSplash.js
+++ src/scripts/TowerSplash.js
@@ -3,8 +3,8 @@ import Tower from './Tower'
 class TowerSplash extends Tower {
     constructor(ctx, coords, type, id, score){
         super(ctx, coords, type, id, score);
-        this.damage = 10;
-        this.speed = 50;
+        this.damage = 100;
+        this.speed = 5;
         this.cost = 300;
         this.score = score;
     }
