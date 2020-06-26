bc2c46c835d5f826b07b5a808ae6ac83ac09eb2e
douglas_crawford <doug.a.crawford@gmail.com>
2020-06-26 16:52:26 -0400

    Splash attacks nearby enemies

--- src/scripts/TowerSplash.js
+++ src/scripts/TowerSplash.js
@@ -12,17 +12,34 @@ class TowerSplash extends Tower {
         super.draw(image);
     }
     attack(enemy, game){
-        debugger
         if (!!enemy){
+            debugger
             const enemyCenter = [enemy.coords[0] + (enemy.coords[2] / 2), enemy.coords[1] + (enemy.coords[3] / 2)];
             const enemyXRange = [enemyCenter[0] - enemy.coords[2], enemyCenter[0] + enemy.coords[2]];
             const enemyYRange = [enemyCenter[1] - enemy.coords[3], enemyCenter[1] + enemy.coords[3]];
 
             const enemies = game.battlefield.enemies;
-            this.ctx.fillStyle = "red";
-            this.ctx.fillRect(enemyXRange[0], enemyYRange[0], (enemy.coords[0] + enemy.coords[2] - enemy.coords[2]), (enemy.coords[1] + enemy.coords[3] - enemy.coords[3]));
+            // this.ctx.fillStyle = "red";
+            // this.ctx.fillRect(enemyXRange[0], enemyYRange[0], enemyXRange[1], enemyYRange[1]);
+            // this.ctx.fillRect(enemyXRange[0], enemyYRange[0], enemyXRange[1], enemyYRange[1]);
+            // this.ctx.fillRect(enemyXRange[0], enemyYRange[0], enemyXRange[1], enemyYRange[1]);
             
+            
+            const nearbyEnemies = Object.values(game.battlefield.enemies)
+                .filter(enemy => {
+                    const otherEnemyCenter = [enemy.coords[0] + (enemy.coords[2] / 2), enemy.coords[1] + (enemy.coords[3] / 2)]
+                    const otherEnemyXRange = [otherEnemyCenter[0] - enemy.coords[2], otherEnemyCenter[0] + enemy.coords[2]];
+                    const otherEnemyYRange = [otherEnemyCenter[1] - enemy.coords[3], enemyCenter[1] + enemy.coords[3]];
+
+                    if ((otherEnemyXRange[0] >= enemyXRange[0] && otherEnemyXRange[1] <= enemyXRange[1]) ||  
+                        (otherEnemyYRange[0] >= enemyYRange[0] && otherEnemyYRange[1] <= enemyYRange[1])) {
+                            return enemy
+                    }
+                });
             super.attack(enemy, game);
+            nearbyEnemies.forEach(enemy => {
+                super.attack(enemy,game);
+            })
         }
     }
 }
